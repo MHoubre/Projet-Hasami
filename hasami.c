c1604651c515e3fb1eef0b910c8e051a45e5c762
Maël Houbre <mael.houbre@telecomnancy.eu>
2017-03-23 08:30:12 +0100

    Pions ajoutés au plateau de jeu

--- hasami.c
+++ hasami.c
@@ -2,29 +2,63 @@
 #include <stdio.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
+#define TailleLigne 9
+#define NbLignes 8
 
 void pause();
 
- 
-int main(int argc, char *argv[]){
+int plateau(){
 
 	int i=0;
+
+// On dessine le plateau
 	SDL_Surface *ecran=NULL; 
 	SDL_Surface *imageDeFond=NULL;
 	SDL_Surface *plateau=NULL;
-	SDL_Surface *lignes[8]={NULL};	
-	SDL_Surface *colonnes[8]={NULL};
+	SDL_Surface *lignes[NbLignes]={NULL};	
+	SDL_Surface *colonnes[NbLignes]={NULL};
 
-	SDL_Rect positionsLignes[8];
-	SDL_Rect positionsColonnes[8];
-	SDL_Rect position1;
-	SDL_Rect position2;
+// On s'occupe des pions
+	SDL_Surface *noirL1[TailleLigne]={NULL}; //première ligne de pions
+	SDL_Surface *noirL2[TailleLigne]={NULL}; // deuxième ligne de pions
+	SDL_Surface *blancL1[TailleLigne]={NULL}; //première ligne de pions
+	SDL_Surface *blancL2[TailleLigne]={NULL}; // deuxième ligne de pions
 
 
-	position1.x=0;
-	position1.y=0;
-	position2.x=25;
-	position2.y=75;
+	SDL_Rect positionsLignes[NbLignes];
+	SDL_Rect positionsColonnes[NbLignes];
+	SDL_Rect positionImage;
+
+	SDL_Rect positionPlateau;
+	SDL_Rect positionNoirL1[TailleLigne];
+	SDL_Rect positionNoirL2[TailleLigne];
+
+
+	SDL_Rect positionBlancL1[TailleLigne];
+	SDL_Rect positionBlancL2[TailleLigne];
+
+
+
+	positionImage.x=0; // Positions d'ancrage de l'image de fond
+	positionImage.y=0; 
+	positionPlateau.x=25; // Positions d'ancrage du plateau de jeu
+	positionPlateau.y=75;
+
+	for(i=0;i<TailleLigne;++i){
+
+		positionNoirL1[i].x=5+i*100;
+		positionNoirL1[i].y=5;
+		positionNoirL2[i].x=5+i*100;
+		positionNoirL2[i].y=105;
+
+		positionBlancL1[i].x=5+i*100;
+		positionBlancL1[i].y=705;
+		positionBlancL2[i].x=5+i*100;
+		positionBlancL2[i].y=805;
+
+	}
+
+	
 
  // Je crée un pointeur sur une SDL_Surface car c'est ce que SetVideoMode renvoie
 	
@@ -33,19 +67,31 @@ int main(int argc, char *argv[]){
     ecran= SDL_SetVideoMode(2000, 1300, 32, SDL_HWSURFACE|SDL_RESIZABLE); // chargement d'une fenêtre dans l'espace écran
 	plateau= SDL_CreateRGBSurface(SDL_HWSURFACE,900,900,32,0,0,0,0);	
 
-	for(i=1;i<9;i++){
+	for(i=0;i<8;i++){
 		lignes[i]= SDL_CreateRGBSurface(SDL_HWSURFACE,900,6,32,0,0,0,0);
 		colonnes[i]= SDL_CreateRGBSurface(SDL_HWSURFACE,6,900,32,0,0,0,0);
 		positionsLignes[i].x=0;
-		positionsLignes[i].y=i*100;
-		positionsColonnes[i].x=i*100;
+		positionsLignes[i].y=(i+1)*100;
+		positionsColonnes[i].x=(i+1)*100;
 		positionsColonnes[i].y=0;
 		SDL_FillRect(lignes[i],NULL,SDL_MapRGB(lignes[i]->format,255,255,255));
 		SDL_FillRect(colonnes[i],NULL,SDL_MapRGB(lignes[i]->format,255,255,255));
 	}
 
 	imageDeFond= IMG_Load("ponton2.jpg");
+
+	for(i=0;i<TailleLigne;i++){
+
+		noirL1[i] = IMG_Load("noire_transparent.png");
+		noirL2[i] = IMG_Load("noire_transparent.png");
+		blancL1[i] = IMG_Load("blanche_transparent.png");
+		blancL2[i] = IMG_Load("blanche_transparent.png");
+	}
+
+
+
 	SDL_FillRect(plateau,NULL,SDL_MapRGB(plateau->format,200,173,127));
+
 
 	if(ecran==NULL){
 		printf("Erreur de chargement du mode vidéo %s\n", SDL_GetError());
@@ -54,15 +100,31 @@ int main(int argc, char *argv[]){
 
 	SDL_WM_SetCaption("Watashi no Hasami Shogi. Dozoo Yorochiku", NULL);
 
-	for(i=0; i<9;i++){
+	for(i=0;i<TailleLigne;i++){
+		SDL_BlitSurface(noirL1[i],NULL,plateau,&positionNoirL1[i]);
+		SDL_BlitSurface(noirL2[i],NULL,plateau,&positionNoirL2[i]);
+		SDL_BlitSurface(blancL1[i],NULL,plateau,&positionBlancL1[i]);
+		SDL_BlitSurface(blancL2[i],NULL,plateau,&positionBlancL2[i]);
+
+	}
+
+
+
+
+
+	for(i=0; i<8;i++){
 
 		SDL_BlitSurface(lignes[i],NULL,plateau,&positionsLignes[i]);
 		SDL_BlitSurface(colonnes[i],NULL,plateau,&positionsColonnes[i]);
 
 	}
 
-	SDL_BlitSurface(imageDeFond,NULL,ecran,&position1);
-	SDL_BlitSurface(plateau,NULL,ecran,&position2);
+
+	SDL_BlitSurface(imageDeFond,NULL,ecran,&positionImage);
+	SDL_BlitSurface(plateau,NULL,ecran,&positionPlateau);	
+
+
+
 
 
 
@@ -70,8 +132,16 @@ int main(int argc, char *argv[]){
     
     pause(); // Mise en pause du programme
 	SDL_FreeSurface(imageDeFond);
+
+	for(i=0;i<TailleLigne;i++){
+		SDL_FreeSurface(noirL1[i]);
+		SDL_FreeSurface(noirL2[i]);
+	}
+
 	SDL_FreeSurface(ecran);
-	for(i=0;i<9;i++){
+
+
+	for(i=0;i<NbLignes;i++){
 		SDL_FreeSurface(colonnes[i]);
 		SDL_FreeSurface(lignes[i]);
 	}
@@ -82,6 +152,17 @@ int main(int argc, char *argv[]){
     return EXIT_SUCCESS; // Fermeture du programme
 }
  
+
+
+
+
+
+ 
+int main(int argc, char *argv[]){
+
+	plateau();
+}
+
 void pause()
 {
     int continuer = 1;
