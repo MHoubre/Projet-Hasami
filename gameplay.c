fec919cee6acc712b5d2024d11fa2818be464c96
BaptisteMonchicourt <baptiste.monchicourt@telecomnancy.eu>
2017-03-27 22:47:40 +0200

    Fonction principale du 1v1 dans gameplay.c et création d'un fichier test.c pour les tests.

--- gameplay.c
+++ gameplay.c
@@ -17,96 +17,96 @@ void initialiser_plateau(Plateau* p) {
 
     int k ;
 
-    for( k = 0 ; k < 81 ; k++){
-        p->cases[k] = VIDE ;
-    }
-
     for (k = 0 ; k < 18 ; k++) {
         p->cases[k] = NOIR ;
     }
 
-    for (k = 80 ; k > 62 ; k--) {
+    for( k = 18 ; k < 63 ; k++){
+        p->cases[k] = VIDE ;
+    }
+
+    for (k = 63 ; k < 81 ; k++) {
         p->cases[k] = BLANC ;
     }
 }
 
 
-void deplacer_piece(Plateau* p, Joueur j, int debut, int fin) { //déplace une pièce depuis la position debut, jusqu'à la position fin
+void deplacer_piece(Plateau* p, Joueur j, int depart, int arrivee) { //déplace une pièce depuis la position depart, jusqu'à la position arrivee
 
-    if (coup_legal(p, j, debut, fin)) {
-            Case piece = (*p).cases[debut] ;
-            p->cases[debut] = VIDE ;
-            p->cases[fin] = piece ;
+    if (coup_legal(p, j, depart, arrivee)) {
+            Case piece = (*p).cases[depart] ;
+            p->cases[depart] = VIDE ;
+            p->cases[arrivee] = piece ;
     }
 }
 
 
-int coup_legal(Plateau* p, Joueur j, int debut, int fin) {
+int coup_legal(Plateau* p, Joueur j, int depart, int arrivee) {
 
     int booleen = 0 ; //booléen déterminant si le coup est légal ou non
-    Case case_depart = (*p).cases[debut] ;
-    Case case_arrivee = (*p).cases[fin] ;
+    Case case_depart = (*p).cases[depart] ;
+    Case case_arrivee = (*p).cases[arrivee] ;
 
-    if (debut>=0 && debut<81) { //la case de départ doit être sur le plateau
+    if (depart>=0 && depart<81) { //la case de départ doit être sur le plateau
 
-        if (fin>=0 && fin<81) { //la case d'arrivée doit être sur le plateau
+        if (arrivee>=0 && arrivee<81) { //la case d'arrivée doit être sur le plateau
 
             if (j+1 == case_depart) { //la pièce doit appartenir au joueur courant
 
-                if (debut != fin) { //les cases de départ et d'arrivée doivent être différentes
+                if (depart != arrivee) { //les cases de départ et d'arrivée doivent être différentes
 
-                    if (debut/9 == fin/9) { //déplacement horizontal
+                    if (depart/9 == arrivee/9) { //déplacement horizontal
 
-                        if (fin>debut) { //déplacement de gauche à droite
-                            int temp = debut+1 ;
+                        if (arrivee>depart) { //déplacement de gauche à droite
+                            int temp = depart+1 ;
 
-                            while ((*p).cases[temp]==VIDE && temp!=fin) {
+                            while ((*p).cases[temp]==VIDE && temp!=arrivee) {
                             temp++ ;
                             }
 
                             //la case d'arrivée doit être vide, et il ne doit pas y avoir de pièce
                             //sur le chemin du déplacement, sauf la case précédent la case d'arrivée
-                            booleen = (fin-temp < 2) && (case_arrivee == VIDE) ;
+                            booleen = (arrivee-temp < 2) && (case_arrivee == VIDE) ;
                         }
 
                         else {
-                            int temp = debut-1 ;
+                            int temp = depart-1 ;
 
-                            while ((*p).cases[temp]==VIDE && temp!=fin) {
+                            while ((*p).cases[temp]==VIDE && temp!=arrivee) {
                             temp-- ;
                             }
 
                             //la case d'arrivée doit être vide, et il ne doit pas y avoir de pièce
                             //sur le chemin du déplacement, sauf la case précédent la case d'arrivée
-                            booleen = (temp-fin < 2) && (case_arrivee == VIDE) ;
+                            booleen = (temp-arrivee < 2) && (case_arrivee == VIDE) ;
                         }
                     }
 
-                    if ((fin-debut)%9 == 0) { //déplacement vertical
+                    if ((arrivee-depart)%9 == 0) { //déplacement vertical
 
-                        if (fin>debut) {
+                        if (arrivee>depart) {
 
-                            int temp = debut+9 ;
+                            int temp = depart+9 ;
 
-                            while ((*p).cases[temp]==VIDE && temp!=fin) {
+                            while ((*p).cases[temp]==VIDE && temp!=arrivee) {
                                 temp+=9 ;
                             }
 
                             //la case d'arrivée doit être vide, et il ne doit pas y avoir de pièce
                             //sur le chemin du déplacement, sauf la case précédent la case d'arrivée
-                            booleen = ((temp == fin) || (temp == fin-9)) && (case_arrivee == VIDE) ;
+                            booleen = ((temp == arrivee) || (temp == arrivee-9)) && (case_arrivee == VIDE) ;
 
                         }
                         else {
-                            int temp = debut-9 ;
+                            int temp = depart-9 ;
 
-                            while ((*p).cases[temp]==VIDE && temp!=fin) {
+                            while ((*p).cases[temp]==VIDE && temp!=arrivee) {
                                 temp-=9 ;
                             }
 
                             //la case d'arrivée doit être vide, et il ne doit pas y avoir de pièce
                             //sur le chemin du déplacement, sauf la case précédent la case d'arrivée
-                            booleen = ((temp == fin) || (temp == fin+9)) && (case_arrivee == VIDE) ;
+                            booleen = ((temp == arrivee) || (temp == arrivee+9)) && (case_arrivee == VIDE) ;
                         }
 
                     }
@@ -142,29 +142,31 @@ int coup_legal(Plateau* p, Joueur j, int debut, int fin) {
     return booleen ;
 }
 
+
 int main(int argc, char *argv[]){
 
+    Joueur joueur_courant = BLANC ;
     Plateau plateau ;
+    int stop = 0 ;
+    int depart ;
+    int arrivee ;
 
     initialiser_plateau(&plateau) ;
 
-    deplacer_piece(&plateau, JOUEUR_NOIR, 0, 19) ;
+    while (stop == 0) {
+        depart = select_depart() ;
+        arrivee = select_arrivee() ;
 
-    deplacer_piece(&plateau, JOUEUR_NOIR, 9, 27) ;
-
-    deplacer_piece(&plateau, JOUEUR_BLANC, 17, 26) ;
-
-    int k ;
-    int zeros = 0 ;
-
-    for (k = 0 ; k < 81 ; k++) {
-;
-        printf("%d\n",plateau.cases[k]) ;
-
-        if (plateau.cases[k]==VIDE){
-            zeros++ ;
+        while (!coup_legal(&p, joueur_courant, depart, arrivee)){
+            depart = select_depart() ;
+            arrivee = select_arrivee() ;
         }
+
+        deplacer_piece(&p, joueur_courant, depart, arrivee) ;
+        joueur_courant = (joueur_courant+1)%2 ;
+
+        afficher(&p) ;
     }
 
-    return zeros ;
+    return 1
 }
