94d5398a6cc1f09d7cc2222b329d29cb40bcf907
Maël Houbre <mael.houbre@telecomnancy.eu>
2017-04-25 11:28:56 +0200

    modification du fichier version_essai. Ajout d'une fonction pour libérer l'espace mémoire du plateau et d'une fonction pour supprimer les pions capturés

--- version_essai.c
+++ version_essai.c
@@ -10,6 +10,8 @@
 #define TailleLigne 9
 #define NbLignes 8
 #define NbPions  36
+#define UneLigne 9
+#define DeuxLignes 18
 
 
 
@@ -42,14 +44,150 @@ void initialiser_plateau(Plateau* p) {
     }
 }
 
+void free_Plateau(Plateau* p){
+	int i;	
 
-void deplacer_piece(Plateau* p, int depart, int arrivee) { //déplace une pièce depuis la position depart, jusqu'à la position arrivee
-
-            Case piece = (*p).cases[depart] ;
-            p->cases[depart] = VIDE ;
-            p->cases[arrivee] = piece ;    
+	for (i=0;i<TailleMatrice;i++){
+		free(&(p->cases[i]));
+	}
 }
 
+
+int coup_legal(Plateau* p, Joueur j, int depart, int arrivee) {
+
+    int booleen = 0 ; //booléen déterminant si le coup est légal ou non
+    Case case_depart = (*p).cases[depart] ;
+    Case case_arrivee = (*p).cases[arrivee] ;
+
+    if (depart>=0 && depart<81) { //la case de départ doit être sur le plateau
+
+        if (arrivee>=0 && arrivee<81) { //la case d'arrivée doit être sur le plateau
+
+            if (j+1 == case_depart) { //la pièce doit appartenir au joueur courant
+
+                if (depart != arrivee) { //les cases de départ et d'arrivée doivent être différentes
+
+                    if (depart/9 == arrivee/9) { //déplacement horizontal
+
+                        if (arrivee>depart) { //déplacement de gauche à droite
+                            int temp = depart+1 ;
+
+                            while ((*p).cases[temp]==VIDE && temp!=arrivee) {
+                            temp++ ;
+                            }
+
+                            //la case d'arrivée doit être vide, et il ne doit pas y avoir de pièce
+                            //sur le chemin du déplacement, sauf la case précédent la case d'arrivée
+                            booleen = (arrivee-temp < 2) && (case_arrivee == VIDE) ;
+                        }
+
+                        else {
+                            int temp = depart-1 ;
+
+                            while ((*p).cases[temp]==VIDE && temp!=arrivee) {
+                            temp-- ;
+                            }
+
+                            //la case d'arrivée doit être vide, et il ne doit pas y avoir de pièce
+                            //sur le chemin du déplacement, sauf la case précédent la case d'arrivée
+                            booleen = (temp-arrivee < 2) && (case_arrivee == VIDE) ;
+                        }
+                    }
+
+                    if ((arrivee-depart)%9 == 0) { //déplacement vertical
+
+                        if (arrivee>depart) {
+
+                            int temp = depart+9 ;
+
+                            while ((*p).cases[temp]==VIDE && temp!=arrivee) {
+                                temp+=9 ;
+                            }
+
+                            //la case d'arrivée doit être vide, et il ne doit pas y avoir de pièce
+                            //sur le chemin du déplacement, sauf la case précédent la case d'arrivée
+                            booleen = ((temp == arrivee) || (temp == arrivee-9)) && (case_arrivee == VIDE) ;
+
+                        }
+                        else {
+                            int temp = depart-9 ;
+
+                            while ((*p).cases[temp]==VIDE && temp!=arrivee) {
+                                temp-=9 ;
+                            }
+
+                            //la case d'arrivée doit être vide, et il ne doit pas y avoir de pièce
+                            //sur le chemin du déplacement, sauf la case précédent la case d'arrivée
+                            booleen = ((temp == arrivee) || (temp == arrivee+9)) && (case_arrivee == VIDE) ;
+                        }
+
+                    }
+
+                    else {
+                        printf("Déplacement non autorisé.\n") ;
+                    }
+
+                }
+                else {
+                    printf("Les cases de départ et d'arrivée doivent être différentes.\n") ;
+                }
+
+            }
+            else {
+                if (case_depart == VIDE) {
+                    printf("La case sélectionnée est vide.\n") ;
+                }
+                else {
+                    printf("La pièce doit appartenir au joueur courant !\n") ;
+                }
+            }
+
+        }
+        else {
+            printf("La case d'arrivée est hors du plateau !\n") ;
+        }
+    }
+    else {
+        printf("La case de départ est hors du plateau !\n") ;
+    }
+
+    return booleen ;
+}
+
+
+
+void deplacer_piece(Plateau* p, Joueur j, int depart, int arrivee) { //déplace une pièce depuis la position depart, jusqu'à la position arrivee
+
+    if (coup_legal(p, j, depart, arrivee)) {
+            Case piece = (*p).cases[depart] ;
+            p->cases[depart] = VIDE ;
+            p->cases[arrivee] = piece ;
+    }
+}
+
+void pris(Plateau* p, int i){
+
+	if ( (*p).cases[i+2]== (*p).cases[i] && (*p).cases[i+1] != (*p).cases[i] && (*p).cases[i+1] != VIDE) {
+		
+		(*p).cases[i+1]=VIDE;
+	}
+
+	if ( (*p).cases[i-2]== (*p).cases[i] && (*p).cases[i-1] != (*p).cases[i] && (*p).cases[i-1] != VIDE) {
+		
+		(*p).cases[i-1]=VIDE;
+	}
+
+	if ( (*p).cases[i+DeuxLignes]== (*p).cases[i] && (*p).cases[i+UneLigne] != (*p).cases[i] && (*p).cases[i+UneLigne] != VIDE) {
+		
+		(*p).cases[i+UneLigne]=VIDE;
+	}
+
+	if ( (*p).cases[i-DeuxLignes]== (*p).cases[i] && (*p).cases[i-UneLigne] != (*p).cases[i] && (*p).cases[i-UneLigne] != VIDE) {
+		
+		(*p).cases[i-UneLigne]=VIDE;
+	}
+
+}
 
 void afficher (Plateau* p){
 
@@ -107,11 +245,15 @@ int main(){
 int i=0;
 
 int compteur=0;
-
 int continuer=1;
+
+Joueur j= JOUEUR_BLANC;
 
 int depart;
 int arrivee;
+
+Plateau pla;
+initialiser_plateau(&pla);
 
 
 	SDL_Surface *ecran=NULL;
@@ -157,8 +299,7 @@ int arrivee;
 
 
 
-	Plateau pla;
-	initialiser_plateau(&pla);
+
 	dessiner(&pla,ecran,pions, positionsPions);
 
 	
@@ -216,13 +357,23 @@ int arrivee;
 
 				arrivee= ((((event.button.y/100)-1)*9)+(event.button.x/100)); // indice d'arrivée
 
-				deplacer_piece(&pla,depart,arrivee); // on déplace la pièce dans la matrice
+				deplacer_piece(&pla, j, depart,arrivee); // on déplace la pièce dans la matrice
+	
+				pris(&pla, arrivee);
+			
 
 				SDL_BlitSurface(imageDeFond,NULL,ecran,&positionImage); // on Reblit tout pour cacher les précédentes places des pions
 
 				SDL_BlitSurface(plateau,NULL,ecran,&positionPlateau);	
 
 				dessiner(&pla, ecran, pions, positionsPions); // on rdéfini  les coordonnées des pions en fonction de leur place dans la matrice de contrôle.
+
+				if (j== JOUEUR_BLANC){  // on passe au joueur suivant
+					j= JOUEUR_NOIR;
+				}else{
+					j= JOUEUR_BLANC;
+				}
+
 
 				break;
 
@@ -246,6 +397,7 @@ int arrivee;
 
 // Si on décide de quitter le jeu (que l'on sort de la boucle while(continuer)), on libère les différents espaces mémoire alloués.    
 
+
 	SDL_FreeSurface(imageDeFond);
 
 	SDL_FreeSurface(ecran);
@@ -259,11 +411,14 @@ int arrivee;
 	for(i=0;i<NbPions;i++){
 		SDL_FreeSurface(pions[i]);
 	}
+
+
  
     SDL_Quit(); // Arrêt de la SDL
  
     return EXIT_SUCCESS; // Fermeture du programme
 
+	free_Plateau(&pla);
 
 }
 
