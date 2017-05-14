19a75feb0df18d45a7ce29a22d25826ddad1b0a1
Maël Houbre <mael.houbre@telecomnancy.eu>
2017-05-14 18:33:45 +0200

    changement du type de retour pieces_joueur

--- min_max.h
+++ min_max.h
@@ -8,6 +8,6 @@ Arbre* initialiser_arbre_coups(Arbre* a, Plateau* p, int pieces[], Joueur joueur
 Plateau* copy_to(Plateau* p, Plateau* p_copy) ;
 int mini(Plateau* p, Arbre* a, Joueur joueur, int profondeur) ;
 int maxi(Plateau* p, Arbre* a, Joueur joueur, int profondeur) ;
-int pieces_joueur(int pieces[], Plateau *p, Joueur joueur) ;
+void pieces_joueur(int pieces[], Plateau *p, Joueur joueur) ;
 
 #endif // MIN_MAX_H_INCLUDED
